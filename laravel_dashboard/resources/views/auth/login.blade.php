0a40e75bd4aca8a255e29ef722356d08eec376e0
lestercch <lester_cch93@mesoamericana.edu.gt>
2022-01-22 23:59:50 -0600

    add Register in Login

--- laravel_dashboard/resources/views/auth/login.blade.php
+++ laravel_dashboard/resources/views/auth/login.blade.php
@@ -1,5 +1,8 @@
 <x-guest-layout>
     <!--Agregamos este link para registrarnos-->
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        <a href="{{route('register')}}" class="ml-4 text-sm text-gray-700 underline"> Register</a>
+    </div>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
